6011fc7fd7c5e833fcbf68d38b5917dcb09f2d98
Алексей Андреев <andreev.psk@yandex.ru>
2023-04-25 21:12:30 +0300

    Добавлены права для автора или админа на редактирование

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -8,3 +8,13 @@ class IsAdminOrSuperUser(permissions.BasePermission):
             request.user.is_authenticated
             and request.user.is_admin
         )
+
+
+class IsAuthorOrAdminOrReadOnly(permissions.BasePermission):
+    """Изменять записи может только их автор."""
+    def has_object_permission(self, request, view, obj):
+        return (
+            obj.author == request.user
+            or request.user.is_admin
+            or request.method in permissions.SAFE_METHODS
+        )
