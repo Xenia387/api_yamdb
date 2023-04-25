c48a5ebb3d21f90100c06761259222e69de5b5f4
Ксения Анисимова <anis.xenia@yandex.ru>
2023-04-25 10:26:31 +0300

    изменение моделей Title, Genre, Category, создание модели GenreTitle

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Category(models.Model):
-    category = models.CharField(
+    name = models.CharField(
         help_text='Название категории не должно быть длиннее 200 символов',
         max_length=200,
         verbose_name='Название категории'
@@ -18,7 +18,7 @@ class Category(models.Model):
 
 
 class Genre(models.Model):
-    genre = models.CharField(
+    name = models.CharField(
         help_text='Название жанра не должно быть длиннее 200 символов',
         max_length=200,
         verbose_name='Название жанра'
@@ -34,33 +34,31 @@ class Genre(models.Model):
 
 
 class Title(models.Model):
-    title = models.CharField(
+    name = models.CharField(
         help_text='Название произведения не должно быть длиннее 500 символов',
         max_length=500,
         verbose_name='Название произведения'
     )
-    author = models.CharField(
-        help_text='Название категории не должно быть длиннее 200 символов',
-        max_length=200,
-        verbose_name='Автор произведения'
-    )
-    pub_date = models.DateTimeField(
+    year = models.DateTimeField(
         verbose_name='Дата выпуска'
-    )
-    description = models.TextField(
-        help_text='Добавьте описание произведения',
-        verbose_name='Описание произведния'
     )
     category = models.OneToOneField(
         Category,
         on_delete=models.SET_NULL,
         verbose_name='Категория произведения'
     ),
-    genre = models.ForeignKey(
-        Genre,
-        on_delete=models.SET_NULL,
-        verbose_name='Жанр(ы) произведения'
-    )
 
     class Meta:
         verbose_name = 'Произведение'
+
+
+class GenreTitle(models.Model):
+    title_id = models.ForeignKey(
+        Title,
+        verbose_name='Произведение'
+    )
+    genre_id = models.ForeignKey(
+        Genre,
+        verbose_name='Жанр(ы)',
+        on_delete=models.SET_NULL,
+    )
