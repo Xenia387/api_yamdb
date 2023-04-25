c062fb755daec36c9f9c65fba2445f0e1b92407a
Алексей Андреев <andreev.psk@yandex.ru>
2023-04-25 11:18:41 +0300

    Приложение users и модель для него

---
+++ api_yamdb/users/models.py
@@ -0,0 +1,30 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+
+    class UserRoles(models.TextChoices):
+        USER = 'user'
+        MODERATOR = 'moderator'
+        ADMIN = 'admin'
+
+    email = models.EmailField(
+        verbose_name='Email адрес',
+        blank=False,
+        unique=True
+    )
+    bio = models.TextField(
+        verbose_name='Биография',
+        blank=True,
+    )
+    role = models.CharField(
+        verbose_name='Роль',
+        max_length=16,
+        choices=UserRoles.choices,
+        default=UserRoles.USER,
+        blank=False
+    )
+
+    class Meta:
+        ordering = ('username',)
