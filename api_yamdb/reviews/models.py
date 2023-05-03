0e0f90bbab2a6543ea278217c78370743164f60c
Ксения Анисимова <anis.xenia@yandex.ru>
2023-05-03 16:50:48 +0300

    модель Comment

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -129,26 +129,26 @@ class Review(models.Model):
         return self.text
 
 
-# class Comment(models.Model):
-#     review = models.ForeignKey(
-#         Review,
-#         on_delete=models.CASCADE,
-#         related_name='comments',
-#     )
-#     text = models.TextField(
-#         help_text='Комментарий не более 1000 символов',
-#         max_length=1000,
-#         verbose_name='Текст комментария',
-#     )
-#     author = models.ForeignKey(
-#         User,
-#         on_delete=models.CASCADE,
-#         related_name='comments',
-#     )
-#     pub_date = models.DateTimeField(
-#         auto_now_add=True,
-#         verbose_name='Дата публикации',
-#     )
+class Comment(models.Model):
+    review = models.ForeignKey(
+        Review,
+        on_delete=models.CASCADE,
+        related_name='comments',
+    )
+    text = models.TextField(
+        help_text='Комментарий не более 1000 символов',
+        max_length=1000,
+        verbose_name='Текст комментария',
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='comments',
+    )
+    pub_date = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Дата публикации',
+    )
 
-#     class Meta:
-#         ordering = ('id', )
+    class Meta:
+        ordering = ('id', )
